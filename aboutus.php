f9ac392ce8470343ec4f28a96e7fc63d89000da0
James Hollowell <jthollo@g.clemson.edu>
2016-11-01 15:40:56 -0400

    Some text for the About Us page

--- aboutus.php
+++ aboutus.php
@@ -10,7 +10,16 @@
 <main>
     <div class="container">
         <h1>About Us</h1>
-        <p></p>
+        <p>NewSite is owned and operated by James Hollowell. With a BS in computer science and 2 years of web design
+            experience, he is highly qualified to lead this company and work for our customers. James started
+            programming at a young age, learning web coding during a summer program at a local library. Since then, he
+            has been fascinated with the world of computer science, constantly striving to be better. Recently, he
+            worked with some small businesses doing odd jobs on their websites, and found out just how much they could
+            benefit from a professionally designed and created website.</p>
+        <p>We love working with small businesses because we are one. We understand how small businesses run and some of
+            the oddities that come with running a small business. We are here to help you achieve <strong>your</strong>
+            goals and reach <strong>your</strong> customers.
+        </p>
     </div>
 </main>
 <?php
